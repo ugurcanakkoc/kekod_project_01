7d7b2d914845d60e636d446de8c065e58c3b108c
Ugur Can <ugurcnakkoc@gmail.com>
2024-09-06 16:35:50 +0300

    Refactor MenuItem from data class to sealed class with objects

--- appcompose/src/main/java/com/example/appcompose/model/MenuItem.kt
+++ appcompose/src/main/java/com/example/appcompose/model/MenuItem.kt
@@ -1,8 +1,18 @@
 package com.example.appcompose.model
 
-data class MenuItem(
-    val switchId: Int,
-    val switchName: String,
-    val destinationId: Int,
-    val itemIcon: Int
-)
+
+import com.example.appcompose.R
+
+sealed class MenuItem(
+    val route: String,
+    val itemIcon: Int,
+    val label: String,
+) {
+    data object Switch : MenuItem("switchList", R.drawable.ic_switches, "Switch")
+    data object Giving : MenuItem("givingScreen", R.drawable.ic_giving, "Giving")
+    data object Happiness : MenuItem("happinessScreen", R.drawable.ic_happiness, "Happiness")
+    data object Respect : MenuItem("respectsScreen", R.drawable.ic_respect, "Respects")
+    data object Kindness : MenuItem("kindnessScreen", R.drawable.ic_kindness, "Kindness")
+    data object Optimism : MenuItem("optimismScreen", R.drawable.ic_optimism, "Optimism")
+    data object Ego : MenuItem("egoScreen", R.drawable.ic_launcher_foreground, "Ego")
+}
